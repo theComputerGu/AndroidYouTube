708d24fae594e8671f8a102d32a33b36a6be6c1c
theComputerGu <marksheinberg01@gmail.com>
2024-06-12 15:12:07 +0300

    Update Post.java

--- app/src/main/java/com/example/myapplication/Post.java
+++ app/src/main/java/com/example/myapplication/Post.java
@@ -8,24 +8,7 @@ import java.io.Serializable;
 public class Post implements Serializable {
     private String content;
     private String author;
-    private Bitmap pic;
-    private Uri videoUri;
-
-    private int likes;
-    private int dislikes;
-    private int shares;
-    private String date;
-
-    public Post(String content, String author, String date, Bitmap pic,Uri videoUri) {
-        this.content = content;
-        this.author = author;
-        this.pic = pic;
-        this.videoUri = videoUri ;
-        this.date = date;
-        this.likes = 0;
-        this.dislikes = 0;
-        this.shares = 0;
-    }
+    private Bitmap bit;
 
     public Uri getVideoUri() {
         return videoUri;
@@ -35,6 +18,58 @@ public class Post implements Serializable {
         this.videoUri = videoUri;
     }
 
+    private Uri videoUri; // Add this field
+    public void setPic(int pic) {
+        this.pic = pic;
+    }
+    int i;
+
+    private int pic;
+    private int likes;
+    private int dislikes;
+    private String date;
+    private int type; // New field to store the type of the post (image or video)
+
+    // Constants to represent the types of posts
+    public static final int TYPE_IMAGE = 1;
+    public static final int TYPE_VIDEO = 2;
+
+
+    public Post(String content, String author, Bitmap bit, String date) {
+        this.content = content;
+        this.author = author;
+        this.bit = bit;
+        this.date = date;
+        this.type = TYPE_IMAGE; // By default, assume it's an image post
+    }
+    public Post(String content, String author, int pic, String date) {
+        this.content = content;
+        this.author = author;
+        this.pic = pic;
+        this.date = date;
+        this.type = TYPE_IMAGE; // By default, assume it's an image post
+    }
+
+    public Post(String content, String author, int pic, String date, Uri videoUri) {
+        this.content = content;
+        this.author = author;
+        this.pic = pic;
+        this.date = date;
+        this.videoUri = videoUri;
+        this.type = TYPE_IMAGE; // By default, assume it's an image post
+    }
+
+
+    // Getter and setter methods for likes, dislikes, and other fields (omitted for brevity)
+
+    // Add getter and setter methods for type and fileName
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
 
     public String getContent() {
         return content;
@@ -44,7 +79,7 @@ public class Post implements Serializable {
         return author;
     }
 
-    public Bitmap getPic() {
+    public int getPic() {
         return pic;
     }
 
@@ -68,10 +103,12 @@ public class Post implements Serializable {
     public void setDislikes(int dislikes) {
         this.dislikes = dislikes;
     }
-    public int getShares() {
-        return shares;
+
+    public Bitmap getBit() {
+        return bit;
     }
-    public void setShares(int shares) {
-        this.shares = shares;
+
+    public void setBit(Bitmap bit) {
+        this.bit = bit;
     }
 }
